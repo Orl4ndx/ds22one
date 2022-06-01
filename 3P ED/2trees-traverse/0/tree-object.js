18d030c476d775d8c723946c1bc2f3f763e08526
Orl4ndx <99366316+Orl4ndx@users.noreply.github.com>
2022-06-01 16:04:16 -0500

    Add files via upload

---
+++ 3P ED/2trees-traverse/0/tree-object.js
@@ -0,0 +1,26 @@
+  <script>
+
+let root={
+  d:'+',
+  l:null,//left
+  r:null//right
+ }
+
+ let n1={
+  d:1,
+  l:null,
+  r:null
+ }
+
+ let n2={
+  d:2,
+  l:null,
+  r:null
+ }
+
+
+
+ root.l=n1
+ root.r=n2
+    
+  </script>
---
+++ 3P ED/2trees-traverse/1/preorder.js
@@ -0,0 +1,10 @@
+  <script>
+
+ function preorder(n){
+  if (n!==null){
+    console.log(n.d)
+    preorder(n.l)
+    preorder(n.r)
+  }
+}
+  </script>
